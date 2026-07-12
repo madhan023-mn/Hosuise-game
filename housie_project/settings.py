af662235188c26d2bcd0224b48f0cb0af7ab24d2
madhan023-mn <madhangosikonda23@gmail.com>
2026-07-12 22:42:51 +0530

    Configure postgres for production

--- housie_project/settings.py
+++ housie_project/settings.py
@@ -90,12 +90,18 @@ AUTH_USER_MODEL = 'accounts.User'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+database_url = os.environ.get('DATABASE_URL')
+if database_url:
+    DATABASES['default'] = dj_database_url.parse(database_url, conn_max_age=600)
 
 
 # Password validation
